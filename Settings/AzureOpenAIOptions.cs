ac121a9e25f5acc5c1236ee4969c364a7e503c4d
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-10-24 02:59:34 +0200

    add settings for AzureOpenAI

---
+++ Settings/AzureOpenAIOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HSB.BE.Settings
+{
+	public class AzureOpenAIOptions
+	{
+		[Required]
+		public string Endpoint { get; set; } = default!;
+
+		[Required]
+		public string ApiKey { get; set; } = default!;
+
+		[Required]
+		public string DeploymentName { get; set; } = default!;
+	}
+}
