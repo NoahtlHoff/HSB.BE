a6943a61bac94ede00eb141e955538d12518e739
Noah <noah.hoff@outlook.com>
2025-11-03 16:20:23 +0100

    swagger in prod

--- Program.cs
+++ Program.cs
@@ -50,14 +50,10 @@ namespace HSB.BE
 
 			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
-			app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
 			app.UseAuthentication();
 			app.UseAuthorization();
