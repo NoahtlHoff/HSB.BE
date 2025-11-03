972766ee1c8c3a6d241ddbf01e3c4026ad3e24e4
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-11-03 23:42:22 +0100

    apply formatting from .editorconfig file

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,12 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HSB.BE.Data
 {
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
 
-        public DbSet<User> Users { get; set; }
-    }
+		public DbSet<User> Users { get; set; }
+	}
 }
