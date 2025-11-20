c586e26044a6316dca73a367e3f094961d7cbd71
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-11-20 23:33:37 +0100

    add methods for getting all conversationNames and all non-summary messages in a conversation

---
+++ src/Controllers/ConversationsController.cs
@@ -0,0 +1,41 @@
+﻿using HSB.BE.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace HSB.BE.Controllers
+{
+	[ApiController]
+	[Route("api/conversations")]
+	public class ConversationsController : ControllerBase
+	{
+		private readonly IConversationMemoryService _memoryService;
+
+		public ConversationsController(IConversationMemoryService service)
+		{
+			_memoryService = service;
+		}
+
+		[Authorize]
+		[HttpGet]
+		public async Task<IActionResult> GetAllConversationNames()
+		{
+			var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			if (userId is null)
+				return Unauthorized();
+			var conversationNames = await _memoryService.GetAllConversationNamesAsync(userId);
+			return Ok(conversationNames);
+		}
+
+		[Authorize]
+		[HttpGet("{conversationId}")]
+		public async Task<IActionResult> GetConversationMessagesById(string conversationId)
+		{
+			var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			if (userId is null)
+				return Unauthorized();
+			var conversation = await _memoryService.GetAllConversationMessagesAsync(conversationId, userId);
+			return conversation != null ? Ok(conversation) : NotFound();
+		}
+	}
+}
