d4d47875ec02e0c79ea44e869101a9c147128b1a
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-11-20 20:19:41 +0100

    add conversationName model

---
+++ src/Models/ConversationName.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace HSB.BE.Models
+{
+	public class ConversationName
+	{
+		[Required]
+		[JsonProperty("id")]
+		public string Id { get; set; } = Guid.NewGuid().ToString();
+
+		[Required]
+		[JsonProperty("userId")]
+		public string UserId { get; set; } = default!;
+
+		[Required]
+		[JsonProperty("conversationId")]
+		public string ConversationId { get; set; } = default!;
+
+		[Required]
+		[JsonProperty("name")]
+		public string Name { get; set; } = default!;
+	}
+}
