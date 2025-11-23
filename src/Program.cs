6e2211debd3c740b7e25c991e558040809acd555
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-11-23 19:55:30 +0100

    used old interface name

--- src/Program.cs
+++ src/Program.cs
@@ -69,7 +69,7 @@ namespace HSB.BE
 				return new CosmosClient(opts.Endpoint, opts.Key);
 			});
 
-			builder.Services.AddSingleton<ICosmoDbContext, ComsoDbContainers>();
+			builder.Services.AddSingleton<ICosmoDbContainers, ComsoDbContainers>();
 
 			builder.Services.AddScoped<IUserRepository, UserRepository>();
 			builder.Services.AddScoped<IChatRepository, ChatRepository>();
