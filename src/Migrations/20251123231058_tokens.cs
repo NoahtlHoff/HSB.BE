298234c8be64b2f43735181818c99063db02b7bf
Noah <noah.hoff@outlook.com>
2025-11-24 00:11:32 +0100

    migrations

---
+++ src/Migrations/20251123231058_tokens.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HSB.BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class tokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ChatTokens",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastTokenResetUtc",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChatTokens",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastTokenResetUtc",
+                table: "Users");
+        }
+    }
+}
