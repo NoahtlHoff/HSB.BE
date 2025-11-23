66ae34505c380a3898a065e0e0a857e3d64f6c3b
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-11-23 18:23:39 +0100

    add cosmoDbContainers for easier dependency injection

---
+++ src/Data/ComsoDbContainers.cs
@@ -0,0 +1,24 @@
+﻿using HSB.BE.Settings;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Options;
+
+namespace HSB.BE.Data
+{
+	public interface ICosmoDbContext
+	{
+		Microsoft.Azure.Cosmos.Container ConversationsContainer { get; }
+		Microsoft.Azure.Cosmos.Container ConversationNamesContainer { get; }
+	}
+	public class ComsoDbContainers : ICosmoDbContext
+	{
+		public Microsoft.Azure.Cosmos.Container ConversationsContainer { get; }
+		public Microsoft.Azure.Cosmos.Container ConversationNamesContainer { get; }
+		public ComsoDbContainers(CosmosClient client, IOptions<CosmosDbOptions> options)
+		{
+			var opt = options.Value;
+			Database database = client.GetDatabase(opt.DatabaseName);
+			ConversationsContainer = database.GetContainer(opt.ConversationsContainerName);
+			ConversationNamesContainer = database.GetContainer(opt.ConversationsContainerName);
+		}
+	}
+}
