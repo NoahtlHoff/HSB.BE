1881873d4253ffa3e3af514009b08e2b1f6a9543
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-11-24 22:38:04 +0100

    add validationHelper

---
+++ tests/ValidationHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace HSB.BE.Tests
+{
+	public static class ValidationHelper
+	{
+		public static List<ValidationResult> ValidateObject(object obj)
+		{
+			var results = new List<ValidationResult>();
+			var context = new ValidationContext(obj, serviceProvider: null, items: null);
+			Validator.TryValidateObject(obj, context, results, validateAllProperties: true);
+			return results;
+		}
+	}
+}
